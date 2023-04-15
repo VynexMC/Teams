7b7fbb883ce4c8435f8d24cbb82e4c1fa3dc44ed
zerqzmc@gmail.com <zerqzmc@gmail.com>
2023-04-15 11:02:10 -0400

    Removed join message stuff because it's unnecessary and makes it too complicated

--- src/main/java/me/goodwilled/teams/listeners/ConnectionListener.java
+++ src/main/java/me/goodwilled/teams/listeners/ConnectionListener.java
@@ -32,37 +32,9 @@ public class ConnectionListener implements Listener {
 
         if (team == Team.CITIZEN) {
             player.sendMessage(ColourUtils.colour("&aLooks like you haven't selected a Team yet. Do &b/teams &a to do so!"));
-            Bukkit.broadcastMessage(ColourUtils.colour("&a" + player.getName() + " &7&ojoined the game."));
         } else {
-            switch (team) {
-                case KNIGHT -> {
-                    Bukkit.getServer().broadcastMessage(ColourUtils.colour("&5&oBe warned... a &3Knight &5&ohas joined the game."));
-                    Bukkit.getServer().broadcastMessage(ColourUtils.colour("&5&oGreetings, &a" + player.getName() + "&5&o."));
-                }
-                case ASSASSIN -> {
-                    Bukkit.getServer().broadcastMessage(ColourUtils.colour("&6&oWatch your back... because an &4Assassin &6&ohas joined the game."));
-                    Bukkit.getServer().broadcastMessage(ColourUtils.colour("&6&oNice to see you, &a" + player.getName() + "&6&o."));
-                }
-                case MAGE -> {
-                    Bukkit.getServer().broadcastMessage(ColourUtils.colour("&3&oA strong breeze blows through the world... because a &2Mage &3&ohas joined the game."));
-                    Bukkit.getServer().broadcastMessage(ColourUtils.colour("&3&oHello, &a" + player.getName() + "&3&o."));
-                }
-                case VIKING -> {
-                    Bukkit.getServer().broadcastMessage(ColourUtils.colour("&c&oCarry a shield with you... because a &6Viking &c&ohas joined the game."));
-                    Bukkit.getServer().broadcastMessage(ColourUtils.colour("&c&oWe missed you, &a" + player.getName() + "&c&o."));
-                }
-            }
-
             team.applyJoinPerks(player);
         }
-    }
-
-    @EventHandler
-    public void on(PlayerQuitEvent event) {
-        final Player player = event.getPlayer();
-
-        this.teamsPlugin.getTeamManager().unload(event.getPlayer().getUniqueId());
-        Bukkit.getServer().broadcastMessage(ColourUtils.colour("&c" + player.getName() + " &7&oleft the game."));
     }
 
 }
